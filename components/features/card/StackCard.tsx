8b44bfdccc5bf96b7c424c6e9b17c62ce0d54176
GregLef <gregorylefrancois.travail@yahoo.com>
2025-12-13 12:10:42 +0100

    feat: finish Stack section

--- components/features/card/StackCard.tsx
+++ components/features/card/StackCard.tsx
@@ -13,9 +13,9 @@ export const StackCard = ({ name, src, alt, width, height }: StackCardProps) => 
       <div className="flex flex-col justify-center items-center bg-primary w-36 h-36 lg:w-80 lg:h-80 rounded-md p-4" style={{
         boxShadow: "10px 10px 4px rgba(0, 0, 0, 0.25)",}}>
         <div className='flex justify-center items-center h-full'>
-           <Image src={src} alt={alt} width={width} height={height} className='w-28 lg:w-64'/> 
+           <Image src={src} alt={alt} width={width} height={height} className='w-24 lg:w-60'/> 
         </div>
-        <h3 className="font-regular text-3xl text-secondary font-bold lg:text-4xl">{name}</h3>
+        <h3 className="font-regular text-2xl text-secondary font-bold lg:text-4xl">{name}</h3>
       </div>
   );
 };
--- components/features/landing/Stack.tsx
+++ components/features/landing/Stack.tsx
@@ -7,8 +7,8 @@ export const Stack = () => {
             <h2 className="font-heading text-secondary text-center text-4xl font-bold">Stack et Compétences</h2>
             <div className="flex flex-col gap-8 lg:flex-row lg:gap-12">
         <StackCard name={"Next.js"} src={"/NextJSLogo.svg"} alt={"Logo Next.js"} height={100} width={100}/>
-        <StackCard name={"Next.js"} src={"/NextJSLogo.svg"} alt={"Logo Next.js"} height={100} width={100}/>
-        <StackCard name={"Next.js"} src={"/NextJSLogo.svg"} alt={"Logo Next.js"} height={100} width={100}/>
+        <StackCard name={"TypeScript"} src={"/TypeScriptLogo.svg"} alt={"Logo TypeScript"} height={80} width={80}/>
+        <StackCard name={"Express"} src={"/ExpressJsLogo.svg"} alt={"Logo Express"} height={100} width={100}/>
             </div>
             <Button variant="primary" href={'#'}>Voir tout</Button>
         </section>
