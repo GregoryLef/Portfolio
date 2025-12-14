3a91037a381df073bd2bf059e71f1692a3647ff3
GregLef <gregorylefrancois.travail@yahoo.com>
2025-12-14 22:15:15 +0100

    refactor: delete useless migrations

--- prisma/migrations/20251213131832_add_path_image_to_project/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "Project" ADD COLUMN "Capture1Url" TEXT;
-ALTER TABLE "Project" ADD COLUMN "Capture2Url" TEXT;
-ALTER TABLE "Project" ADD COLUMN "Capture3Url" TEXT;
-ALTER TABLE "Project" ADD COLUMN "Capture4Url" TEXT;
-ALTER TABLE "Project" ADD COLUMN "Capture5Url" TEXT;
-ALTER TABLE "Project" ADD COLUMN "landingUrl" TEXT;
